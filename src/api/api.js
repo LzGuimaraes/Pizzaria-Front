87e54c405d9a5f10c43f9b986f92130d96cd4329
Luiz <131723272+LzGuimaraes@users.noreply.github.com>
2026-05-24 12:32:48 -0400

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // URL do seu backend
+  baseURL: 'https://pizzaria-hymw.onrender.com', // URL do seu backend
 });
 
 // Antes de cada requisição, injeta o token se existir
